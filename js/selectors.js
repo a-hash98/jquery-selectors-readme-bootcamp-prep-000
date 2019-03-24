44151207811cd7f70d2ae0ccc8b0ba8e0097178d
Ay Has <ahashim983@yahoo.com>
2019-03-24 20:46:18 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,21 @@
 $('img')
+function paragraphSelector(){
+  $('p')
+}
 
+function lastImageSelector(){
+  $('img:last')
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  $('.pics')
+}
+
+function firstListItem(){
+  $('ul pic-list:first-child')
+}
 // declare your functions here...
